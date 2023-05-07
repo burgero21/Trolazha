d98ed5d91adeb6990ffbd40edc45f7b10995fc3b
burgero21 <132840666+burgero21@users.noreply.github.com>
2023-05-08 00:17:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -12,17 +12,26 @@ function reshi() {
   while (lugje.length != 0) {
     const randomIndex = Math.floor(Math.random() * lugje.length);
     const choek = lugje[randomIndex];
-    if (choek.toLowerCase == "petar"){
-       lugje.splice(randomIndex, 1)
+    if (choek.toLowerCase() == "petar") {
+      lugje.splice(randomIndex, 1);
+      setTimeout(function () {
+        petardetected();
+      }, 100);
+    } else {
+      const para = document.createElement("p");
+      const node = document.createTextNode(brojac + "." + " " + choek);
+      const element = document.getElementById("div1");
+      element.appendChild(para);
+      lugje.splice(randomIndex, 1);
+      brojac = brojac + 1;
+      para.appendChild(node);
     }
-    else{
+  }
+  function petardetected() {
     const para = document.createElement("p");
-    const node = document.createTextNode(brojac + "." + " " + choek);
+    const node = document.createTextNode(brojac + "." + " Petar");
     const element = document.getElementById("div1");
     element.appendChild(para);
-    lugje.splice(randomIndex, 1);
-    brojac = brojac + 1;
-    para.appendChild(node)
-   }
+    para.appendChild(node);
   }
 }
